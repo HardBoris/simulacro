e215df835e979bb839fd45e38d1c638cbbe01d18
HardBoris <bgaibor03@gmail.com>
2023-08-23 12:05:42 -0400

    feat: urna

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const botones = ["branco", "cancela", "confirma"];
+  const digitos = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["0"]];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="urna">
+        <div className="urna-display"></div>
+        <div className="urna-teclas">
+          <div className="urna-teclas-logo">
+            <h1>JUSTIÇA</h1>
+            <h1>ELEITORAL</h1>
+          </div>
+          <div className="urna-teclado">
+            <div className="teclado-digitos">
+              {digitos.map((item, index) => (
+                <div key={index} className="filas">
+                  {item.map((digito) => (
+                    <div key={digito} className="columnas">
+                      {digito}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+            <div className="teclado-botonera">
+              {botones.map((item, index) => (
+                <div key={index} className={item}>
+                  {item}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
